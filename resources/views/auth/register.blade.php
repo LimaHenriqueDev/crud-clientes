93b21be585a6e73e1fd15a51314976eca07a09d3
henriquelima <henriquelimasousadev@gmail.com>
2025-08-07 15:42:54 -0300

    Ajusta titulos

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,7 +2,7 @@
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8" />
-    <title>Cadastro</title>
+    <title>Cadastrar</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
 </head>
 @extends('layouts.app')
@@ -11,7 +11,7 @@
 
 <div class="container d-flex justify-content-center align-items-center vh-100">
     <div class="col-md-5 bg-white p-4 rounded shadow">
-        <h2 class="mb-4 text-center">Cadastro de Usuário</h2>
+        <h2 class="mb-4 text-center">Cadastrar Usuário</h2>
 
         @if(session('success'))
             <div class="alert alert-success">{{ session('success') }}</div>
